7514318bd1cd203719721a27d1c0b6a46f2c637b
Alexandra <alu0100505122@ull.edu.es>
2013-11-14 03:55:33 -0800

    the end

--- SesionPresencialIyII/2-Entregables/act3.py
+++ SesionPresencialIyII/2-Entregables/act3.py
@@ -8,33 +8,36 @@ import os
 import math 
 import sympy
 from sympy import *
-
-funcions = ["math.e**x", "math.e**((-2*x)**2)", "cos(x)", "ln(x)"]
-points = [0, 0, 2 * math.pi, 1]
-h = 0.01
+from sympy.abc import x, y
 
 def error(a, b):
-	return abs(int(b) - int(a))
+	return abs(b-a)
 
-def f(f, a):
-	return eval(str(f).replace("x", "a"))
+def diff(f, x, h):
+	return ( f(x + h) - f(x - h)) / 2 * h
 
-def diff(fx, x, h):
-	return ( f(fx, x + h) - f(fx, x - h)) / 2 * h
 
 def main():
-	os.system('clear')
-	x = Symbol("x")
-	for fx, p in zip(funcions, points):
-		print ' ------------------------------------------------------------ '
-		print ' --- Para la fucion = ', fx , ' en x = ', p, ' y h = ', h
-		a = diff(fx, p, h)
-		print ' --- My diff es     = ', a
-		b = sympy.diff(fx, x)
-		print ' --- Python diff es = ', b
-		c = error(a, b) 
-		print ' -------------> Error = ', c
-		print ' ------------------------------------------------------------- '
-		
 
+	points = [0, 0, 2 * math.pi, 1]
+	h = 0.01
+	x = symbols('x')
+	fucions = [math.e**x, math.e**((-2*x)**2), cos(x), ln(x)]
+	os.system('clear')
+	print ' ------------------------------------------------------------ '
+	print ' ------       Resultados de las operaciones        ---------- '
+	print ' ------------------------------------------------------------ '
+	
+	for f, p in zip(fucions, points):
+		print ' ------------------------------------------------------------ '
+		print ' --- Para la fucion = ', f
+		print ' --- ( en x = ', p, ' y h = ', h, " )   "
+		a = diff(f, p, h)
+		print ' --- My diff es     = ', a
+		b = sympy.diff(f).subs(x, p)
+		print ' --- Python diff es = ', b
+		print ' -----------> Error = ', error(a, b) 
+		print ' ------------------------------------------------------------ '
+	
+	
 main()
